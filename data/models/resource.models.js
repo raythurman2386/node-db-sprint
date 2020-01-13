013314ffc31ee7ea530dbbdc11083ccb7137122d
raythurman2386 <raymondthurman5@gmail.com>
2020-01-12 19:48:16 -0500

    resource model complete

---
+++ data/models/resource.models.js
@@ -0,0 +1,22 @@
+const db = require('../../data/db-config');
+
+// Get Resource
+function getResources() {
+  return db('resource');
+}
+
+// Add Resource
+function addResource(resource) {
+  return db('resource').insert(resource);
+}
+
+// Delete Resource
+function deleteResource(id) {
+  return db('resource').where({ id }).del();
+}
+
+module.export = {
+  getResources,
+  addResource,
+  deleteResource
+}
